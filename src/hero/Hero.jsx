990af8a64c257fdadfcbac4e13cf50e3eac40420
sohaibshaltaf <sohaibshaltafaltrefe@gmail.com>
2026-01-17 23:48:36 +0300

    Hero Section Summoning Modification

--- src/hero/Hero.jsx
+++ src/hero/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './Hero.module.css';
+import styles from './hero.module.css';
 import logo from '../assets/image/avataaars.svg';
 
 export default function Hero() {
