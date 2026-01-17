6c13ff129c9cc699d3a450cc6e333c3e675264bd
sohaibshaltaf <sohaibshaltafaltrefe@gmail.com>
2026-01-17 15:18:33 +0300

    create componant hero

--- src/hero/Hero.jsx
+++ src/hero/Hero.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from 'react';
+import styles from './Hero.module.css';
+import logo from '../assets/image/avataaars.svg';
 
 export default function Hero() {
   return (
-    <div id='Hero' className=''>Hero</div>
-  )
+    <section id="hero" className={`${styles.hero}  vh-100 d-flex align-items-center`}>
+      <div className="container text-center text-white d-flex flex-column justify-content-center align-items-center  ">
+
+        <img src={logo} alt="avatar" className="w-25 mb-4" />
+
+        <h1 className="">
+          Start Bootstrap
+        </h1>
+
+        <div className={`${styles.divider} my-4`}>
+          <span className={styles.line}></span>
+          <i className={`bi bi-star-fill ${styles.star}`}></i>
+          <span className={styles.line}></span>
+        </div>
+
+        <h3 className=" ">
+          Graphic Artist - Web Designer - Illustrator
+        </h3>
+
+      </div>
+    </section>
+  );
 }
--- src/portfolio/Portfolio.jsx
+++ src/portfolio/Portfolio.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Portfolio() {
   return (
-   <p>Portfolio</p>
+   <p>Portfolio
+    
+   </p>
   )
 }
