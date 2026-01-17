80459c08fcdae983ffbb8ec46ae97b66c8027993
sohaibshaltaf <sohaibshaltafaltrefe@gmail.com>
2026-01-17 07:57:29 +0300

    Create app

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from './navbar/Navbar'
+import Hero from './hero/Hero'
+import Portfolio from './portfolio/Portfolio'
+import About from './about/About'
+import Contact from './contact/Contact'
+import Footer from './footer/Footer'
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Portfolio />
+      <About />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
